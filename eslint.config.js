11128ea759ad42a19f67df693a77f3253c4b32a5
ShinobiWPS <axanderghemov@gmail.com>
2024-02-15 21:41:30 +0100

    chore(config): expand ESlint, add githooks for code and commit linting

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+/** @type {import('eslint').Linter.FlatConfig[]} */
+import antfu from '@antfu/eslint-config'
+import tailwind from 'eslint-plugin-tailwindcss'
+import JsxA11y from 'eslint-plugin-jsx-a11y'
+
+export default antfu(
+
+  {
+    typescript: {
+      tsconfigPath: 'tsconfig.json',
+    },
+    react: true,
+    // svelte: true,
+    // unocss: true,
+    stylistic: {
+      'jsx-one-expression-per-line': 'off',
+    },
+    ignores: [
+      '**/fixtures',
+    ],
+  },
+
+  {
+    files: ['**/*.{js,ts,tsx,jsx}'],
+    plugins: {
+      JsxA11y,
+      tailwind,
+    },
+    rules: { },
+  },
+)
