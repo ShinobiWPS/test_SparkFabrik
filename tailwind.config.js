f93bc646ef3e9cd1ef931a8aeb3fbbbf23b01bee
Alex Ghera <axanderghemov@gmail.com>
2024-02-16 19:03:49 +0100

    chore(libraries): enable usage of DaisyUI/Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
     'node_modules/daisyui/dist/**/*.js',
     'node_modules/react-daisyui/dist/**/*.js',
   ],
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['cmyk', 'night'],
+    darkTheme: 'night',
+    utils: true,
+    themeRoot: ':root',
+  },
 }
