2989c0cb9b09493c63474835f386c46282406518
Alex Ghera <axanderghemov@gmail.com>
2024-02-18 20:16:52 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { Button } from 'react-daisyui'
+import { Button, Form, Input } from 'react-daisyui'
 import './index.css'
+import SignInButton from './entities/auth/sign-in.component'
 
 function App() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    // Handle form submission logic here
+  }
+  const isLoggedIn = false
+
   return (
     <>
       <Button color="primary">Click me!</Button>
+      { !isLoggedIn && (
+        <SignInButton />
+      ) }
+      { isLoggedIn && (
+        <Form onSubmit={handleSubmit}>
+          <Input placeholder="quote here.." />
+          <Input placeholder="author" />
+          <Button type="submit" color="primary">Submit</Button>
+        </Form>
+      ) }
     </>
   )
 }
