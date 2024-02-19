a7bfc0a8ceefaac9752fb908243ec289ff7f86d6
Alexandru Gherasim <ShinobiWPS@users.noreply.github.com>
2024-02-19 15:58:45 +0100

    Develop (#1)
    * chore(config): allow VScode settings for the project
    
    * chore(libraries): enable usage of DaisyUI/Tailwind
    
    * feat: add authentication via Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Form, Input } from 'react-daisyui'
+import './index.css'
+
+import AuthButton from './entities/auth/auth.component'
+import { useAuth } from './entities/auth/auth.context'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { currentUser, loading } = useAuth()
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+  }
+
+  if (loading)
+    return <div>Loading...</div>
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AuthButton />
+      { currentUser && (
+        <Form onSubmit={handleSubmit}>
+          <Input placeholder="quote here.." />
+          <Input placeholder="author" />
+          <Button type="submit" color="primary">Submit</Button>
+        </Form>
+      ) }
     </>
   )
 }
---
+++ src/entities/auth/auth.context.tsx
@@ -0,0 +1,39 @@
+import type { ReactNode } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
+import type { User } from 'firebase/auth'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+interface AuthContextValue {
+  currentUser: User | null
+  loading: boolean
+}
+const AuthContext = createContext<AuthContextValue | null>(null)
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+interface AuthProviderProps {
+  children: ReactNode
+}
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [currentUser, setCurrentUser] = useState<User | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const auth = getAuth()
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user)
+      setLoading(false)
+    })
+
+    return () => unsubscribe()
+  }, [])
+
+  const value = {
+    currentUser,
+    loading,
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
